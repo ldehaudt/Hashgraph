a742d89bd6e5bf9e4fab35e1ae9b39123d8548a4
Conan Wu <cwu@e1z2r4p34.42.us.org>
2018-08-03 21:00:26 -0700

    change hashgraph to pointers

--- main.cpp
+++ main.cpp
@@ -13,11 +13,11 @@ void    printHash(Person* p){
 	}
 }
 
-void square(Event e)
+void square(Event *e)
 {
 	SDL_Rect rect;
-	int y = 1000 - e.getTimestamp() / 1500;
-	int x = 100 + e.getOwner().index * 800 / (N - 1);
+	int y = 1000 - e->getTimestamp() / 1500;
+	int x = 100 + e->getOwner().index * 800 / (N - 1);
 	rect.w = 20;
 	rect.h = 20;
 	rect.x = x - 10;
@@ -25,12 +25,12 @@ void square(Event e)
 	SDL_RenderFillRect(rend, &rect);
 }
 
-void connect(Event e, Event p)
+void connect(Event *e, Event *p)
 {
-	int y = 1000 - e.getTimestamp() / 1500;
-	int x = 100 + e.getOwner().index * 800 / (N - 1);
-	int y2 = 1000 - p.getTimestamp() / 1500;
-	int x2 = 100 + p.getOwner().index * 800 / (N - 1);
+	int y = 1000 - e->getTimestamp() / 1500;
+	int x = 100 + e->getOwner().index * 800 / (N - 1);
+	int y2 = 1000 - p->getTimestamp() / 1500;
+	int x2 = 100 + p->getOwner().index * 800 / (N - 1);
 	SDL_RenderDrawLine(rend, x, y, x2, y2);
 }
 
@@ -45,12 +45,12 @@ void refresh(Person *p)
 	for (unsigned int i = 0; i < p->getHashgraph().size(); i++)
 	{
 		square(p->getHashgraph()[i]);
-		if (((p->getHashgraph())[i]).getSelfParent())
+		if (((p->getHashgraph())[i])->getSelfParent())
 		{
-			std::cout << "STUFF : "<< ((p->getHashgraph())[i]).tVal << std::endl;
-			std::cout << "STUFF : "<< ((p->getHashgraph())[i]).getSelfParent() << std::endl;
-			std::cout << "STUFF : "<< ((p->getHashgraph())[i]).getSelfParent()->tVal << std::endl;
-			connect(p->getHashgraph()[i], *(((p->getHashgraph())[i]).getSelfParent()));
+			std::cout << "STUFF : "<< ((p->getHashgraph())[i])->tVal << std::endl;
+			std::cout << "STUFF : "<< ((p->getHashgraph())[i])->getSelfParent() << std::endl;
+			std::cout << "STUFF : "<< ((p->getHashgraph())[i])->getSelfParent()->tVal << std::endl;
+			connect(p->getHashgraph()[i], *(((p->getHashgraph())[i])->getSelfParent()));
 			//connect(p->getHashgraph()[i], *(((p->getHashgraph())[i]).getGossiperParent()));
 		}
 	}
@@ -83,7 +83,7 @@ std::cout << startTime << std::endl;
 		people[i]->gossip(*(people[j]));
 		printHash(people[j]);
 		for (int i = 0; i < people[j]->getHashgraph().size(); i++)
-			std::cout << "TREE 1 : " << &(people[j]->getHashgraph()[i]) << std::endl;
+			std::cout << "TREE 1 : " << people[j]->getHashgraph()[i] << std::endl;
 		refresh(people[j]);
 		sleep(1);
 		SDL_PollEvent(&event);
